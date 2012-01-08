52772f9c71b5c35c8706ee9587457e6357af4092
Don Zaragoza <donzaragoza1975@gmail.com>
2012-01-08 12:03:17 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hms::Application.config.secret_token = 'c6d69b41c064cc71935d487cfc183704c531a2953a9a81249ff877feb594b0fe294c9bead32a6335031a1d5dee87cf8e63dc0cd1a2c5a7eac3de7abe8c47d202'
